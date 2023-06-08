588c9fec04effc6a7376939ade0f7432b3aef932
RoNaranjo02 <130687333+RoNaranjo02@users.noreply.github.com>
2023-06-07 23:37:50 -0300

    Add files via upload

---
+++ interfaceR.py
@@ -0,0 +1,15 @@
+from zope.interface import Interface
+
+class IEmpleado(Interface):
+
+    def insertarElemento(posicion):
+        pass
+        
+
+    def agregarElemento(elemento):
+        pass
+
+    def mostrarElemento(posicion):
+        pass
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+from menu import menuOpciones
+from manejadorEmpleados import controlEmpleados
+
+
+def test():
+    listaempleados = controlEmpleados()
+    listaempleados.cargarEmpleados()
+    menu = menuOpciones()
+    menu.opciones(listaempleados)
+
+
+if __name__ == "__main__":
+    test()
